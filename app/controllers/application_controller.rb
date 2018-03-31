42e7426090d09566273dfb90132a959d0c98bc51
jekaserov0804 <jekaserov0804@gmail.com>
2018-03-31 18:22:52 +0300

    auth without warden

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,14 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+  helper_method :current_user
+
+  private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def authorize
+    redirect_to log_in_url, notice: 'Non authorized' if current_user.nil?
+  end
+end
